063175c23ad3d411b2ccfd32cb7df5c08e8c676f
Abhiseka Dash <abhisekadash123@gmail.com>
2019-10-30 11:05:44 +0530

    'newchanges'

--- Database1.py
+++ Database1.py
@@ -4,8 +4,6 @@ database="myquiz",
 user = "postgres",
 password = "b1903aed1d43f99f0afc8975190147390f805e061e824beb4ba4a106316e78dc",
 host = "ec2-54-197-238-238.compute-1.amazonaws.com",
-URL="postgres://yriywxkxtklqar:b1903aed1d43f99f0afc8975190147390f805e\
-061e824beb4ba4a106316e78dc@ec2-54-197-238-238.compute-1.amazonaws.com:5432/d2j8ncse346lns",
 port = "5432")
 mycursor=conn.cursor()
 # Insert account informtion.
